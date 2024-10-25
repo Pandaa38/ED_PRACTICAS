1f670b71d8bac326fd346378a086958191e498bc
Pandaa38 <juanrechea@gmail.com>
2024-10-25 23:18:44 +0200

    stack hecho

--- PRACTICA4_lineales/TDA-PilasColasPrado_Sin/TDA-PilasColasPradoMio/estudiante/include/maxstack.h
+++ PRACTICA4_lineales/TDA-PilasColasPrado_Sin/TDA-PilasColasPradoMio/estudiante/include/maxstack.h
@@ -1,5 +1,37 @@
+
+//#ifndef MAXSTACK_H
+//#define MAXSTACK_H
+
+#include <iostream>
+#include <queue>
+
 /**
  * @file maxstack.h
  * @brief  Archivo de especificación del TDA MaxStack
  * @author
  */
+
+struct element {
+  int value;
+  int max_value;
+};
+
+class MaxStack {
+
+private:
+    std::queue<element> elements;
+    std::priority_queue<int> maxValues;
+
+/**************************************************************************************/
+public:
+    void push(int value);
+    void pop();
+
+    element top();
+    int max();
+
+    int size();
+    bool empty();
+};
+
+std::ostream& operator<<(std::ostream& os, const element& elem);
--- PRACTICA4_lineales/TDA-PilasColasPrado_Sin/TDA-PilasColasPradoMio/estudiante/src/maxstack.cpp
+++ PRACTICA4_lineales/TDA-PilasColasPrado_Sin/TDA-PilasColasPradoMio/estudiante/src/maxstack.cpp
@@ -1,5 +1,63 @@
+
+#include "maxstack.h"
+
+
+
 /**
  * @file maxstack.cpp
  * @brief  Archivo de implementación del TDA MaxStack
  * @author
  */
+
+void MaxStack::push(int value) {
+
+    int maxVal = maxValues.empty() ? value : std::max(value, maxValues.top());
+
+    maxValues.push(maxVal);
+    elements.push({value, maxVal});
+}
+void MaxStack::pop() { //hay que eliminar el ultimo y cambiar si necesario el maxValue
+    if (elements.empty()) {
+        throw std::out_of_range("ERROR (pop): La pila está vacía.");
+    }
+
+    if(elements.front().value == maxValues.top()) maxValues.pop();
+
+    std::queue<element> tempQueue;
+    while (elements.size() > 1) {
+        tempQueue.push(elements.front());
+        elements.pop();
+    }
+    // Eliminar el último elemento (que queda en `q`).
+    elements.pop();
+
+    // Reemplazar la cola original con la auxiliar
+    elements = tempQueue;
+}
+
+element MaxStack::top() {
+    if (elements.empty()) {
+        throw std::out_of_range("ERROR (top): La pila está vacía.");
+    }
+    return elements.back();
+}
+
+int MaxStack::max() {
+    if (elements.empty()) { //da igual cual revise por que van a la par de elementos
+        throw std::out_of_range("ERROR (max): La pila está vacía.");
+    }
+    return maxValues.top();
+}
+
+int MaxStack::size() {
+    return elements.size();
+}
+
+bool MaxStack::empty() {
+    return elements.empty();
+}
+
+std::ostream& operator<<(std::ostream& os, const element& elem) {
+    os << elem.value << ", " << elem.max_value;
+    return os;
+}
